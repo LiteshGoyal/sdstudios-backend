ef6bf0cc71ea8e0879d713496974757a0d78e326
Sameer <liteshgoyal55@gmail.com>
2025-01-02 21:05:15 +0530

    updated buildsh

--- build.sh
+++ build.sh
@@ -1,12 +1,16 @@
 set -o errexit
 
+echo "Installing dependencies..."
 pip install -r requirements.txt
 
+echo "Collecting static files..."
 python manage.py collectstatic --no-input
 
+echo "Running migrations..."
 python manage.py migrate
 
-if [ "$CREATE_SUPERUSER" = "1" ];
-then
-    python manage.py createsuperuser --no-input
+echo "Checking if superuser should be created..."
+if [ "$CREATE_SUPERUSER" = "1" ]; then
+    echo "Creating superuser..."
+    python manage.py createsuperuser --no-input || echo "Superuser creation failed."
 fi
