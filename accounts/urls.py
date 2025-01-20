e4c9df989d1b6246a70314a31ffdb6dd82a78ca8
Sameer <liteshgoyal55@gmail.com>
2025-01-20 15:59:03 +0530

    Update user

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('signin/', views.SignInView.as_view(), name='signin'),
     path('signout/', views.SignOutView.as_view(), name='signout'),
     path('current-user/', views.current_user_view, name='current_user'),
+    path('user/update/', views.UserUpdateView.as_view(), name='user-update'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -7,9 +7,10 @@ from rest_framework.authtoken.models import Token
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
+from django.contrib.auth.password_validation import validate_password
 
 class SignUpView(APIView):
-    permission_classes = [AllowAny]  # Allow anyone to access this view
+    permission_classes = [AllowAny] 
 
     def post(self, request):
         username = request.data.get('username')
@@ -52,3 +53,38 @@ def current_user_view(request):
         "username": request.user.username,
         "email": request.user.email,
     })
+
+class UserUpdateView(APIView):
+    permission_classes = [IsAuthenticated] 
+
+    def get(self, request):
+        """
+        GET method to retrieve the current user's information (username and email).
+        """
+        user = request.user
+        return Response({
+            "username": user.username,
+            "email": user.email
+        })
+
+    def put(self, request):
+        """
+        PUT method to update the current user's username or password.
+        """
+        user = request.user
+
+        username = request.data.get('username', None)
+        password = request.data.get('password', None)
+
+        if username:
+            user.username = username
+        
+        if password:
+            try:
+                validate_password(password)
+                user.set_password(password)
+            except Exception as e:
+                return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+        
+        user.save()
+        return Response({"message": "User updated successfully"}, status=status.HTTP_200_OK)
